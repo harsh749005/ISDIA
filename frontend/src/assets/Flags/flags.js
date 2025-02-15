e3990f760cb865c0111dc1c36616862ad0c5bc9b
Harsh <patelharsh749005@gmail.com>
2025-02-15 14:19:47 +0530

    infiniteScroll has some changes and arry object is made for flages

---
+++ frontend/src/assets/Flags/flags.js
@@ -0,0 +1,58 @@
+import india from './india.png';
+import australia from './australia.png';
+import china from './china.png';
+import france from './france.png';
+import ireland from './ireland.png';
+import korea from './korea.png';
+import malaysia from './malaysia.png';
+import morocco from './morocco.png';
+import moldova from './moldova.png';
+import nigeria from './nigeria.png';
+import norway from './norway.png';
+import russia from './russia.png';
+import saudi_arabia from './saudi-arabia.png';
+import spain from './spain.png';
+import taiwan from './taiwan.png';
+import usa from './usa.png';
+import vietnam from './vietnam.png';
+import yemen from './yemen.png';
+import mauritius from './mauritius.png';
+import netherlands from './netherlands.png';
+import south_africa from './south-africa.png';
+import thailand from './thailand.png';
+import czech_republic from './czech-republic.png';
+import switzerland from './switzerland.png';
+import sweden from './sweden.png';
+import uae from './uae.png';
+
+const flags = [
+    { name: 'india', img: india },
+    { name: 'australia', img: australia },
+    { name: 'china', img: china },
+    { name: 'france', img: france },
+    { name: 'ireland', img: ireland },
+    { name: 'south-korea', img: korea },
+    { name: 'malaysia', img: malaysia },
+    { name: 'morocco', img: morocco },
+    { name: 'moldova', img: moldova },
+    { name: 'nigeria', img: nigeria },
+    { name: 'norway', img: norway },
+    { name: 'russia', img: russia },
+    { name: 'saudi-arabia', img: saudi_arabia },
+    { name: 'korea', img: korea },
+    { name: 'spain', img: spain },
+    { name: 'taiwan', img: taiwan },
+    { name: 'usa', img: usa },
+    { name: 'vietnam', img: vietnam },
+    { name: 'yemen', img: yemen },
+    { name: 'mauritius', img: mauritius },
+    { name: 'netherlands', img: netherlands },
+    { name: 'south-africa', img: south_africa },
+    { name: 'thailand', img: thailand },
+    { name: 'czech-republic', img: czech_republic },
+    { name: 'switzerland', img: switzerland },
+    { name: 'sweden', img: sweden },
+    { name: 'uae', img: uae },
+];
+
+export default flags;
--- frontend/src/components/InfiniteScroll/InfiniteScroll.jsx
+++ frontend/src/components/InfiniteScroll/InfiniteScroll.jsx
@@ -1,41 +1,26 @@
-import React from 'react'
-import './InfiniteScroll.css'
+import React from 'react';
+import './InfiniteScroll.css';
+import flags from '../../assets/Flags/flags';
+
 const InfiniteScroll = () => {
   return (
-
-    <div >
-    <div className="wrapper flex gap-5 ">
-        <div className="item item1 min-w-32 h-16 relative" style={{"--position":1}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/india.png" alt="india" /></div>
-        <div className="item item2 min-w-32 h-16 relative" style={{"--position":2}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/australia.png" alt="australia" /></div>
-        <div className="item item3 min-w-32 h-16 relative" style={{"--position":3}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/china.png" alt="china" /></div>
-        <div className="item item4 min-w-32 h-16 relative" style={{"--position":4}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/france.png" alt="france" /></div>
-        <div className="item item5 min-w-32 h-16 relative" style={{"--position":5}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/ireland.png" alt="ireland" /></div>
-        <div className="item item6 min-w-32 h-16 relative" style={{"--position":6}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/korea.png" alt="southkorea" /></div>
-        <div className="item item7 min-w-32 h-16 relative" style={{"--position":7}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/malaysia.png" alt="malaysia" /></div>
-        <div className="item item8 min-w-32 h-16 relative" style={{"--position":8}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/morocco.png" alt="morocco" /></div>
-        <div className="item item9 min-w-32 h-16 relative" style={{"--position":9}}><img  className="w-full h-full object-cover absolute" src="https://www.isdia.org/img/i_image/moldova.png" alt="moldova" /></div>
-        <div className="item item10 min-w-32 h-16 relative" style={{"--position":10}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/nigeria.png" alt="nigeria" /></div>
-        <div className="item item11 min-w-32 h-16 relative" style={{"--position":11}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/norway.png" alt="norway" /></div>
-        <div className="item item12 min-w-32 h-16 relative" style={{"--position":12}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/russia.png" alt="russia" /></div>
-        <div className="item item13 min-w-32 h-16 relative" style={{"--position":13}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/saudi-arabia.png" alt="saudia arabia" /></div>
-        <div className="item item14 min-w-32 h-16 relative" style={{"--position":14}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/korea.png" alt="korea" /></div>
-        <div className="item item15 min-w-32 h-16 relative" style={{"--position":15}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/spain.png" alt="spain" /></div>
-        <div className="item item16 min-w-32 h-16 relative" style={{"--position":16}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/taiwan.png" alt="taiwan" /></div>
-        <div className="item item17 min-w-32 h-16 relative" style={{"--position":17}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/united-states-america.png" alt="usa" /></div>
-        <div className="item item18 min-w-32 h-16 relative" style={{"--position":18}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/vietnam.png" alt="vietnam" /></div>
-        <div className="item item19 min-w-32 h-16 relative" style={{"--position":19}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/yemen.png" alt="yemen" /></div>
-        <div className="item item20 min-w-32 h-16 relative" style={{"--position":20}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/mauritius.png" alt="mauritius" /></div>
-        <div className="item item21 min-w-32 h-16 relative" style={{"--position":21}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/netherlanda.png" alt="netherlands" /></div>
-        <div className="item item22 min-w-32 h-16 relative" style={{"--position":22}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/south-africa.png" alt="south africa" /></div>
-        <div className="item item23 min-w-32 h-16 relative" style={{"--position":23}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/thailand.png" alt="thailand" /></div>
-        <div className="item item24 min-w-32 h-16 relative" style={{"--position":24}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/czekh-republic.png" alt="czekh republic" /></div>
-        <div className="item item25 min-w-32 h-16 relative" style={{"--position":25}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/switzerland.png" alt="switzerland" /></div>
-        <div className="item item26 min-w-32 h-16 relative" style={{"--position":26}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/sweden.png" alt="sweden" /></div>
-        <div className="item item27 min-w-32 h-16 relative" style={{"--position":27}}><img className="w-full h-full object-cover absolute"  src="https://www.isdia.org/img/i_image/uae.png" alt="uae" /></div>
+    <div>
+      <div className="wrapper flex gap-5">
+        {flags.map((flag, index) => (
+          <div
+            key={index}
+            className={`item item${index + 1} min-w-32 h-16 relative`}
+          >
+            <img
+              className="w-full h-full object-cover absolute"
+              src={flag.img}
+              alt={flag.name}
+            />
+          </div>
+        ))}
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default InfiniteScroll
+export default InfiniteScroll;
